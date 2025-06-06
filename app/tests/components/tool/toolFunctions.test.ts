9e655ac0f39d05db8ad15850071f3be772e11b3a
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2025-06-06 19:24:51 +0200

    Configured for component testing

---
+++ app/tests/components/tool/toolFunctions.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { 
+    putColor, 
+    isValidInput,
+    copyStyle,
+    putStyle,
+    chooseColor  
+} from "~/components/tool/toolFunctions";
+
+describe("Tool component functions", () => {
+    it("");
+});
